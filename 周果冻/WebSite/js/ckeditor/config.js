62333f3b60b53b661abadef9d0c04d7dff68f00a
周国栋 <742782908@qq.com>
2016-06-07 14:05:47 +0800

    加入测试页面，配置好CKeditor的宽高和工具栏

--- 周果冻/WebSite/js/ckeditor/config.js
+++ 周果冻/WebSite/js/ckeditor/config.js
@@ -7,4 +7,16 @@ CKEDITOR.editorConfig = function (config) {
     // Define changes to default configuration here. For example:
     // config.language = 'fr';
     // config.uiColor = '#AADC6E';
+    config.image_previewText = ' '; //预览区域显示内容
+    config.filebrowserImageUploadUrl = "../admin_handler/upload_file.ashx"; //待会要上传的action或servlet
+    config.height = 400; //设置高度。
+    config.width = 750; //设置宽度。
+
+    config.toolbar = 'Full'; config.toolbar_Full = [
+        { name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', '-', 'RemoveFormat'] },
+        { name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'] },
+        { name: 'styles', items: ['Font', 'FontSize'] },
+        { name: 'colors', items: ['TextColor', 'BGColor'] },
+        { name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar'] },
+        { name: 'tools', items: ['Maximize'] }];
 };
