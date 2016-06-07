b221538ca1f2db8f24643d26bd0d9ecd114dad62
周国栋 <742782908@qq.com>
2016-06-07 18:39:55 +0800

    上传文件例子

---
+++ 周果冻/WebSite/html/upload_file.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="upload_file.aspx.cs" Inherits="html_upload_file" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form enctype="multipart/form-data" method="POST" dir="ltr" lang="zh-cn" action="upload_file.aspx">
+        <input type="file" name="upload" />
+        <input type="submit" />
+    </form>
+</body>
+</html>
---
+++ 周果冻/WebSite/html/upload_file.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Web;
+
+public partial class html_upload_file : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        HttpPostedFile file = Request.Files["upload"]; //获取上传的文件
+        if (file == null)
+            return;
+        string filename = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName); //将文件名设置成guid值，避免重复
+        string filepath = "../upload_files/" + filename; //生成文件路径
+        //filename = GetMD5HashFromFile(file);
+        string manpath = Server.MapPath(filepath); //用context.Server.MapPath()生成绝对路径
+
+        try
+        {
+            file.SaveAs(manpath); //保存文件，SaveAs()方法需要绝对路径
+        }
+        catch (Exception ex)
+        {
+            filename = null;
+            Response.Write(ex.Message);
+            return;
+        }
+
+        Response.Write("上传成功");
+    }
+}
