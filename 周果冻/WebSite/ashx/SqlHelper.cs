55f7d41e6b9929ce9f9b7f81857668aa8adef428
周国栋 <742782908@qq.com>
2016-06-29 22:16:16 +0800

    json、Helper 可用，添加类库

--- 周果冻/WebSite/ashx/SqlHelper.cs
+++ 周果冻/WebSite/ashx/SqlHelper.cs
@@ -1,103 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Data;
-using System.Linq;
-using System.Web;
 
-namespace CRUDTest1
+public static class SqlHelper
 {
-    public static class SqlHelper
+    public static readonly string connstr =
+        ConfigurationManager.ConnectionStrings["connstr"].ConnectionString;
+
+    public static SqlConnection OpenConnection()
     {
-        public static readonly string connstr =
-            ConfigurationManager.ConnectionStrings["connstr"].ConnectionString;
+        SqlConnection conn = new SqlConnection(connstr);
+        conn.Open();
+        return conn;
+    }
 
-        public static SqlConnection OpenConnection()
+    public static int ExecuteNonQuery(string cmdText,
+        params SqlParameter[] parameters)
+    {
+        using (SqlConnection conn = new SqlConnection(connstr))
         {
-            SqlConnection conn = new SqlConnection(connstr);
             conn.Open();
-            return conn;
+            return ExecuteNonQuery(conn, cmdText, parameters);
         }
+    }
 
-        public static int ExecuteNonQuery(string cmdText,
-            params SqlParameter[] parameters)
+    public static object ExecuteScalar(string cmdText,
+        params SqlParameter[] parameters)
+    {
+        using (SqlConnection conn = new SqlConnection(connstr))
         {
-            using (SqlConnection conn = new SqlConnection(connstr))
+            conn.Open();
+            return ExecuteScalar(conn, cmdText, parameters);
+        }
+    }
+
+    public static DataTable ExecuteDataTable(string cmdText,
+        params SqlParameter[] parameters)
+    {
+        using (SqlConnection conn = new SqlConnection(connstr))
+        {
+            conn.Open();
+            return ExecuteDataTable(conn, cmdText, parameters);
+        }
+    }
+
+    public static int ExecuteNonQuery(SqlConnection conn, string cmdText,
+       params SqlParameter[] parameters)
+    {
+        using (SqlCommand cmd = conn.CreateCommand())
+        {
+            cmd.CommandText = cmdText;
+            cmd.Parameters.AddRange(parameters);
+            return cmd.ExecuteNonQuery();
+        }
+    }
+
+    public static object ExecuteScalar(SqlConnection conn, string cmdText,
+        params SqlParameter[] parameters)
+    {
+        using (SqlCommand cmd = conn.CreateCommand())
+        {
+            cmd.CommandText = cmdText;
+            cmd.Parameters.AddRange(parameters);
+            return cmd.ExecuteScalar();
+        }
+    }
+
+    public static DataTable ExecuteDataTable(SqlConnection conn, string cmdText,
+        params SqlParameter[] parameters)
+    {
+        using (SqlCommand cmd = conn.CreateCommand())
+        {
+            cmd.CommandText = cmdText;
+            cmd.Parameters.AddRange(parameters);
+            using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
             {
-                conn.Open();
-                return ExecuteNonQuery(conn, cmdText, parameters);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                return dt;
             }
         }
+    }
 
-        public static object ExecuteScalar(string cmdText,
-            params SqlParameter[] parameters)
-        {
-            using (SqlConnection conn = new SqlConnection(connstr))
-            {
-                conn.Open();
-                return ExecuteScalar(conn, cmdText, parameters);
-            }
-        }
+    public static object ToDBValue(this object value)
+    {
+        return value == null ? DBNull.Value : value;
+    }
 
-        public static DataTable ExecuteDataTable(string cmdText,
-            params SqlParameter[] parameters)
-        {
-            using (SqlConnection conn = new SqlConnection(connstr))
-            {
-                conn.Open();
-                return ExecuteDataTable(conn, cmdText, parameters);
-            }
-        }
-
-        public static int ExecuteNonQuery(SqlConnection conn, string cmdText,
-           params SqlParameter[] parameters)
-        {
-            using (SqlCommand cmd = conn.CreateCommand())
-            {
-                cmd.CommandText = cmdText;
-                cmd.Parameters.AddRange(parameters);
-                return cmd.ExecuteNonQuery();
-            }
-        }
-
-        public static object ExecuteScalar(SqlConnection conn, string cmdText,
-            params SqlParameter[] parameters)
-        {
-            using (SqlCommand cmd = conn.CreateCommand())
-            {
-                cmd.CommandText = cmdText;
-                cmd.Parameters.AddRange(parameters);
-                return cmd.ExecuteScalar();
-            }
-        }
-
-        public static DataTable ExecuteDataTable(SqlConnection conn, string cmdText,
-            params SqlParameter[] parameters)
-        {
-            using (SqlCommand cmd = conn.CreateCommand())
-            {
-                cmd.CommandText = cmdText;
-                cmd.Parameters.AddRange(parameters);
-                using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
-                {
-                    DataTable dt = new DataTable();
-                    adapter.Fill(dt);
-                    return dt;
-                }
-            }
-        }
-
-        public static object ToDBValue(this object value)
-        {
-            return value == null ? DBNull.Value : value;
-        }
-
-        public static object FromDBValue(this object dbValue)
-        {
-            return dbValue == DBNull.Value ? null : dbValue;
-        }
+    public static object FromDBValue(this object dbValue)
+    {
+        return dbValue == DBNull.Value ? null : dbValue;
     }
 }
--- 周果冻/WebSite/ashx/get_info.ashx
+++ 周果冻/WebSite/ashx/get_info.ashx
@@ -4,47 +4,39 @@ using System;
 using System.Web;
 using System.Data;
 using System.Web.Script.Serialization;
-using CRUDTest1;
+using DAL;
 
-public class get_info : IHttpHandler {
+public class get_info : IHttpHandler
+{
 
-    public void ProcessRequest (HttpContext context) {
+    public void ProcessRequest(HttpContext context)
+    {
         context.Response.ContentType = "text/html";
 
-        HttpCookie login = context.Request.Cookies["login"];
-        if (login == null)
+        string username = "zogodo";
+        
+        DataTable dt = SqlHelper.ExecuteDataTable("select * from Table_People where Username='" + username + "'");
+        object info = new object();
+        DataRow row = dt.Rows[0];
+        info = new
         {
-            context.Response.SetCookie(new HttpCookie("login", "no"));
-            login = context.Request.Cookies["login"];
-            context.Response.Write("have not login");
-        }
-        else if (login.Value != "no")
-        {
-            DataTable dt = SqlHelper.ExecuteDataTable("select * from Table_People where Username='" + login.Value + "'");
-            object info = new object();
-            DataRow row = dt.Rows[0];
-            info = new
-            {
-                username = row["Username"],
-                password = row["Password"],
-                name = row["Name"],
-                studentid = row["StudentID"],
-                department = row["Department"],
-                post = row["Post"],
-                phone = row["Phone"]
-            };
+            username = row["Username"],
+            password = row["Password"],
+            name = row["Name"],
+            studentid = row["StudentID"],
+            department = row["Department"],
+            post = row["Post"],
+            phone = row["Phone"]
+        };
 
-            string json = new JavaScriptSerializer().Serialize(info);
-            context.Response.Write(json);
-        }
-        else
-        {
-            context.Response.Write("不明错误！");
-        }
+        string json = new JavaScriptSerializer().Serialize(info);
+        context.Response.Write(json);
     }
 
-    public bool IsReusable {
-        get {
+    public bool IsReusable
+    {
+        get
+        {
             return false;
         }
     }
